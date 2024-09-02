70e265871fb046ac1875cffa66a9a07808a7f9c8
pdrofelicio-prog <pedro.felicio.silva@gmail.com>
2024-09-02 18:15:19 -0300

    Delete portfolio directory

--- portfolio/src/components/Title/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-import { Props } from '.'
-
-export const Title = styled.h3<Props>`
-  color: ${(props) => props.theme.primaryColor};
-  font-size: ${(props) => (props.fontSize ? props.fontSize + 'px' : '14px')};
-  font-weight: bold;
-  margin-bottom: 16px;
-`
